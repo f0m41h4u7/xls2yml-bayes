df1798e306a113db0b9db3eb006d99aa86454903
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-02 13:22:12 +0300

    implemented convertion

--- bayes.py
+++ bayes.py
@@ -5,12 +5,14 @@ from nltk.corpus import stopwords
 from nltk.stem.snowball import RussianStemmer
 from pymystem3 import Mystem
 import numpy
+import xml.etree.ElementTree as et
+import datetime
+import json
 
 df = pd.read_excel(open('the_table.xls', 'rb'))
 classes = df.columns
 mystem = Mystem()
 p = [dict() for x in range(len(classes))]
-dlength = 0
 
 def isunique(w):
 	for t in range(0, len(classes)):
@@ -26,21 +28,61 @@ def word_process(cell):
 	return words
 
 def count_freq(words, cnt):
-	global dlength
 	for w in words:
 		if w in p[cnt]:
 			p[cnt][w]+=1
 		else:
-			if isunique(w):
-				dlength += 1
 			p[cnt][w]=1
 
-############ MAIN ############
-def main():
+########### CONVERT ###########
+def convert(indexes):
+	df_work = pd.read_excel(open('the_table.xls', 'rb'))
+	with open('user_info.json', 'r') as f:
+		user_info = json.load(f)
+
+	# create file structure of yml
+	yml_catalog = et.Element('yml_catalog')
+	yml_catalog.set('date', datetime.datetime.now().strftime())
+	shop = et.SubElement(yml_catalog, 'shop')
+
+	name = et.SubElement(shop, 'name')
+	name.text = user_info["name"]
+	company = et.SubElement(shop, 'company')
+	company.text = user_info["company"]
+	url = et.SubElement(shop, 'url')
+	url.text = user_info["url"]
+	currency = et.SubElement(shop, 'currency')
+	currency.set('id', 'RUB')
+	currency.set('rate', '1')
+
+	offers = et.SubElement(shop, 'offers')
+	offer = []
+	subitems = [[0 for x in range(len(df.index))] for y in range(len(classes))] 
+	for i in range(0, len(df.index)):
+		offer[i] = et.SubElement(offers, 'offer')
+		offer[i].set('id', i)
+		for cnt in range(0, len(classes)):
+			subitems[i][cnt] = et.SubElement(offer[i], classes[cnt])
+			subitems[i][cnt].text = df.at[i, classes[indexes[i]]]
+	data = et.tostring(yml_catalog)
+	resultfile = open("table!!.yml", "w")	
+	resultfile.write(data)
+
+############ WORK #############
+def work():
+        df_work = pd.read_excel(open('the_table.xls', 'rb'))
+	classes = df.columns
+	for cnt in range(0, len(classes)):
+                for row in df.index:
+                        cell = df.at[row, classes[cnt]]
+	
+
+############ LEARN ############
+def learn():
 	for cnt in range(0, len(classes)):
 		for row in df.index:
 			cell = df.at[row, classes[cnt]]
-			if not isinstance(cell, int) and not isinstance(cell, numpy.int64):
+			if not isinstance(cell, int) and not isinstance(cell, numpy.int64) and not isinstance(cell, float):
 				cell = cell.encode('utf-8')
 				words = word_process(cell)
 				count_freq(words, cnt)	
@@ -50,8 +92,13 @@ def main():
 				else:
 					p[cnt]['number'] = 1
 		for w in p[cnt]:
-			#p[cnt][w] /= len(p[cnt])
-			print(p[cnt][w], len(p[cnt]))
+			# p[cnt][w] /= len(p[cnt])
+			print(p[cnt][w], len(p[cnt]))		
+
+############ MAIN #############
+def main():
+	learn()
+#	work()
 
 if __name__== "__main__":
 	main()
