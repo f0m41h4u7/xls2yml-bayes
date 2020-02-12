e3310f97f9170f8b9b11dbd9295afcfcb1511b48
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-13 00:04:13 +0300

    some fixes

--- learn.py
+++ learn.py
@@ -1,28 +1,15 @@
 import aerospike
 import pandas as pd
-import redis
+import datetime
+from pandas import Timestamp
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import RussianStemmer
 from pymystem3 import Mystem
 import numpy as np
 
-df = pd.read_excel(open('ks.xlsx', 'rb'))
-classes = df.columns
-mystem = Mystem()
-
-config = {
-    'hosts': [
-        ( '127.0.0.1', 4000 )
-    ],
-    'policies': {
-        'timeout': 1000 # milliseconds
-    }
-}
-client = aerospike.client(config)
-
 def isunique(w):
 	for cls in classes:
-		(key, meta) = client.exists(('vocabulary', cls, w))
+		(key, meta) = client.exists(('test', cls, w))
 		if meta!='None':
 			return False
 	return True
@@ -36,29 +23,50 @@ def word_process(cell):
 
 def count_freq(words, cls):
 	for w in words:
-		(key, meta) = client.exists(('vocabulary', cls, w))
+		(key, meta) = client.exists(('test', cls, w))
 		if meta!='None':
-			client.increment(('vocabulary', cls, w), 1)
+			client.increment(('test', cls, w), 'qty', 1)
 		else:
-			client.put(('vocabulary', cls, w), 1)
+			client.put(('test', cls, w), {'qty': 1})
 
-def learn():
-	for cls in classes:
-		for row in df.index:
-			cell = df.at[row, cls]
-			if not isinstance(cell, int) and not isinstance(cell, np.int64) and not isinstance(cell, float) and not isinstance(cell, TimeStamp):
-				cell = cell.encode('utf-8')
-				words = word_process(cell)
-				count_freq(words, cls)
+# Open dataset table
+df = pd.read_excel(open('ks.xlsx', 'rb'))
+classes = df.columns
+mystem = Mystem()
+
+# Connect to aerospike db
+config = {
+    'hosts': [
+        ( '127.0.0.1', 3000 )
+    ],
+    'policies': {
+        'timeout': 1000
+    }
+}
+client = aerospike.client(config)
+client.connect()
+
+# Start learning
+for cls in classes:
+	for row in df.index:
+		cell = df.at[row, cls]
+		if not isinstance(cell, int) and not isinstance(cell, np.int64) and not isinstance(cell, float) and not isinstance(cell, Timestamp) and not isinstance(cell, datetime.time) and not isinstance(cell, long):
+			cell = cell.encode('utf-8')
+			words = word_process(cell)
+			count_freq(words, cls)
+		elif isinstance(cell, Timestamp) or isinstance(cell, datetime.time):
+			(key, meta) = client.exists(('test', cls, 'datetime'))
+                        if meta!='None':
+                                client.increment(('test', cls, 'datetime'), 'qty', 1)
+                        else:
+                                client.put(('test', cls, 'datetime'), {'qty': 1})
+		else:
+			(key, meta) = client.exists(('test', cls, 'number'))
+			if meta!='None':
+				client.increment(('test', cls, 'number'), 'qty', 1)
 			else:
-				(key, meta) = client.exists(('vocabulary', cls, 'number'))
-				if meta!='None':
-					client.increment(('vocabulary', cls, 'number'), 1)
-				else:
-					client.put(('vocabulary', cls, 'number'), 1)
-#		for w in p[cnt]:
-			# p[cnt][w] /= len(p[cnt])
-#			print(vocabulary[cnt][w], len(vocabulary[cnt]))
+				client.put(('test', cls, 'number'), {'qty': 1})
+#	for w in p[cnt]:
+		# p[cnt][w] /= len(p[cnt])
+#		print(test[cnt][w], len(test[cnt]))
 
-#if __name__ == "__main__":
-#	learn()
